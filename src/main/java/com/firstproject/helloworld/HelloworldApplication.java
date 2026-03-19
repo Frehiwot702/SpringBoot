a775215322223810a88703496fc84869e7983b2c
Frehiwot702 <frehiwot.tewodros112@gmail.com>
2026-03-19 16:52:57 -0600

    first commit

---
+++ src/main/java/com/firstproject/helloworld/HelloworldApplication.java
@@ -0,0 +1,19 @@
+package com.firstproject.helloworld;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@SpringBootApplication
+public class HelloworldApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloworldApplication.class, args);
+	}
+
+	@GetMapping
+	public String greet(){
+		return "Hello there, how u doin?";
+	}
+
+}
